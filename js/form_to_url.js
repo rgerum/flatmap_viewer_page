c4250a55bae2dd112724f38336bab612fa7116f7
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2023-10-30 13:41:03 -0400

    fixed form_to_url.js for empty arrays

--- js/form_to_url.js
+++ js/form_to_url.js
@@ -1,28 +1,37 @@
 
 function form_to_url(form) {
     const formData = new FormData(form);
-        const groupedData = {};
+    console.log("formData", formData)
+    const groupedData = {};
 
-        formData.forEach((value, key) => {
-            if (!groupedData[key]) {
-                groupedData[key] = [];
-            }
-            groupedData[key].push(value);
-        });
+    for(let input of document.getElementsByTagName("input")) {
+        groupedData[input.name] = [];
+    }
 
-        const paramsArray = [];
-
-        for (const [key, values] of Object.entries(groupedData)) {
-            if (values.length > 1) {
-                paramsArray.push(`${key}=${values.join(',')}`);
-            } else {
-                paramsArray.push(`${key}=${values[0]}`);
-            }
+    formData.forEach((value, key) => {
+        if (!groupedData[key]) {
+            groupedData[key] = [];
         }
+        groupedData[key].push(value);
+    });
 
-        const paramString = paramsArray.join('&');
-        const newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${paramString}`;
-        history.pushState(null, '', newUrl);
+    const paramsArray = [];
+
+    for (const [key, values] of Object.entries(groupedData)) {
+        if (values.length > 1) {
+            paramsArray.push(`${key}=${values.join(',')}`);
+        }
+        else if(values.length === 1) {
+            paramsArray.push(`${key}=${values[0]}`);
+        }
+        else {
+            paramsArray.push(`${key}=`);
+        }
+    }
+
+    const paramString = paramsArray.join('&');
+    const newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${paramString}`;
+    history.pushState(null, '', newUrl);
 }
 function link_forms() {
     console.log("link_forms", document.querySelector('form'))
@@ -36,7 +45,11 @@ function restore_form_from_url() {
 
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     checkboxes.forEach((checkbox) => {
-        checkbox.checked = false;
+        console.log("checkboxes", checkbox.name, searchParams.get(checkbox.name), searchParams.has(checkbox.name))
+        if (searchParams.has(checkbox.name)) {
+            console.log("uncheck", checkbox)
+            checkbox.checked = false;
+        }
     });
 
     searchParams.forEach((value, key) => {
@@ -46,7 +59,7 @@ function restore_form_from_url() {
             let val = values[0];
             const input = document.querySelector(`[name="${key}"]`);
             if (input && (input.type === 'checkbox' || input.type === 'radio')) {
-                input.checked = true;
+                input.checked = val != "";
             } else if (input) {
                 input.value = val;
             }
